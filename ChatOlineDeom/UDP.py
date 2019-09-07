3bedde87e595351e615a2c4840d828257a47f134
FreeToBelieve <53510330+FreeToBelieve@users.noreply.github.com>
2019-09-07 21:34:58 +0800

    Add files via upload

---
+++ ChatOlineDeom/UDP.py
@@ -0,0 +1,18 @@
+"""
+@author 
+@create 2019-09-07-16:33
+"""
+from socket import *
+
+
+class UDP(object):
+    def __init__(self):
+        self.addr = ('10.25.115.164', 6066)
+        self.socket = socket(type=SOCK_DGRAM)
+        self.socket.bind(self.addr)
+
+    def udp_send(self, data, addr):
+        self.socket.sendto(data, addr)
+
+    def upd_receive(self):
+        return self.socket.recvfrom(1024)
